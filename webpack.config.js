08bd7efe93e1717874c0430533ad49cb83ee724d
bogdan.kotarlic <bogdan.kotarlic@tiwgroup.com>
2022-12-05 13:36:33 +0100

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
@@ -42,6 +43,11 @@ module.exports = {
         new HtmlWebpackPlugin({
             template:'./public/index.html',
             filename:'index.html'
+        }),
+        new CopyWebpackPlugin({ 
+            patterns:[
+              {from: 'src/images', to:'images'}
+            ]
         }),
         new MiniCssExtractPlugin({
             filename:'assets/[name].css'
